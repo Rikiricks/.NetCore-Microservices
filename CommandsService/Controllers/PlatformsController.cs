e6b49467875d63ca71e7c4bbdff7192433e2b689
Rikiricks <riky92.ce@gmail.com>
2023-06-17 17:21:00 +0530

    Add the ingress-nginx and pvc storage class

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController: ControllerBase
+    {
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
+        }
+    }
+}

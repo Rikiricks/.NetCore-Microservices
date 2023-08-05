8a0952979d891e3ac7bfb3f0980446f5e25555db
Rikiricks <riky92.ce@gmail.com>
2023-08-05 17:26:13 +0530

    Add the message subcriber

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,9 +1,17 @@
+using AsyncDataServices.MessageBusSubscriber;
+using CommandsService.EventProcessing;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHostedService<MessageBusSubscriber>();
+
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
